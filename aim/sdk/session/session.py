08ff8cb18efd03dadeda2cf736efba260a8de589
Neeraj Kashyap <nkashy1@gmail.com>
2021-03-22 06:22:26 -0700

    Set up excepthook in Aim Session

--- aim/sdk/session/session.py
+++ aim/sdk/session/session.py
@@ -6,6 +6,7 @@ from typing import Optional, Dict, List, Tuple
 from queue import Queue, Empty
 
 from aim.engine.repo import AimRepo
+from aim.cli.reporting.reporter import aim_reporter, aim_tags
 from aim.artifacts.artifact_writer import ArtifactWriter
 from aim.sdk.session.utils import (
     exception_resistant,
@@ -34,6 +35,7 @@ class Session:
                  flush_frequency: int = DEFAULT_FLUSH_FREQUENCY,
                  block_termination: bool = True,
                  run: Optional[str] = None):
+        aim_reporter.setup_excepthook(publish=True, tags=aim_tags)
         self.active = False
         self._lock = threading.Lock()
         self._close_lock = threading.Lock()
